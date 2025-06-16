fbef1f09e728b222204686e664a17edcdb432f17
Jan Tvrdík <git@jantvrdik.com>
2025-06-16 11:38:10 +0200

    setup ci

--- tests/RulesetSortingTest.php
+++ tests/RulesetSortingTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace ShipmonkTests\CodingStyle;
+namespace ShipMonkTests\CodingStandard;
 
 use PHPUnit\Framework\TestCase;
 use function implode;
@@ -13,17 +13,14 @@ class RulesetSortingTest extends TestCase
     public function testRulesAreSortedAlphabetically(): void
     {
         $rulesetPath = __DIR__ . '/../ShipMonkCodingStandard/ruleset.xml';
-        $this->assertFileExists($rulesetPath);
+        self::assertFileExists($rulesetPath);
 
         $xml = simplexml_load_file($rulesetPath);
         self::assertNotFalse($xml, 'Failed to parse ruleset.xml');
 
         $rules = [];
-        foreach ($xml->rule as $rule) {
-            $ref = (string) $rule['ref'];
-            if (!empty($ref)) {
-                $rules[] = $ref;
-            }
+        foreach ($xml->rule ?? [] as $rule) {
+            $rules[] = (string) $rule['ref'];
         }
 
         self::assertNotEmpty($rules, 'No rules found in ruleset.xml');
